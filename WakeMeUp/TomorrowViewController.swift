a0a802a8e5cb8065e366a0574d00c90af161dc21
Paul Fynxx <paulvanluling@gmail.com>
2015-06-04 00:12:08 +0200

    Tomorrow View Controller v1

--- WakeMeUp/TomorrowViewController.swift
+++ WakeMeUp/TomorrowViewController.swift
@@ -6,4 +6,19 @@
 //  Copyright (c) 2015 Paul van Luling. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class TomorrowViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
